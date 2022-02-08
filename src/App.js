67c34530f9a6d48b36e92c2451344460264a329d
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-02-08 18:51:43 -0300

    exercicios

--- src/App.js
+++ src/App.js
@@ -1,45 +1,94 @@
 import React from "react";
+// EXERCICIES 1
+// const luana = {
+//   cliente: 'Luana',
+//   idade: 27,
+//   compras: [
+//    {nome: 'Notebook', preco: 'R$2500'},
+//    {nome: 'Geladeira', preco: 'R$3000'},
+//    {nome: 'Smarthone', preco: 'R$1500'},
+//   ],
+//   ativa: true,
+// };
 
-const luana = {
-  cliente: 'Luana',
-  idade: 27,
-  compras: [
-   {nome: 'Notebook', preco: 'R$2500'},
-   {nome: 'Geladeira', preco: 'R$3000'},
-   {nome: 'Smarthone', preco: 'R$1500'},
-  ],
-  ativa: true,
-};
+// const mario = {
+//   cliente: 'Mario',
+//   idade: 31,
+//   compras: [
+//    {nome: 'Notebook', preco: 'R$2500'},
+//    {nome: 'Geladeira', preco: 'R$3000'},
+//    {nome: 'Smarthone', preco: 'R$1500'},
+//    {nome: 'Guitarra', preco: 'R$3500'},
+//   ],
+//   ativa: false,
+// }
 
-const mario = {
-  cliente: 'Mario',
-  idade: 31,
-  compras: [
-   {nome: 'Notebook', preco: 'R$2500'},
-   {nome: 'Geladeira', preco: 'R$3000'},
-   {nome: 'Smarthone', preco: 'R$1500'},
-   {nome: 'Guitarra', preco: 'R$3500'},
-  ],
-  ativa: false,
-}
+// EXERCICIES 2
+// const produtos = [
+//   {
+//     id: 1,
+//     nome: 'Smartphone',
+//     preco: 'R$ 2000',
+//     cores: ['#29d8d5', '#252a34', '#fc3766']
+//   },
+//   {
+//     id: 2,
+//     nome: 'Notebook',
+//     preco: 'R$ 3000',
+//     cores: ['#^ff5045', '#d4394b', '#f37c59']
+//   },
+//   {
+//     id: 3,
+//     nome: 'Tablet',
+//     preco: 'R$ 1500',
+//     cores: ['#365069', '#47c1c8', '#f95786']
+//   }
+
+// ]
+
+
 
 
 const App = () => {
-  const dados = mario
-  const total = dados.compras.map((item) => 
-    Number(item.preco.replace("R$", ""))
-  ).reduce((a , b) => a+b)
+  // EXERCICIES 2
+  // const dados = produtos.filter(
+  //   ({preco}) => Number(preco.replace("R$", "")) > 1500,
+  // );
 
-  
 
-  return ( <div>
-  <p>Nome: {dados.cliente} </p> 
-  <p>Idade: {dados.idade} </p>
-  <p>Situaçao: <span style={{ color: dados.ativa ? 'green' : 'red'}}> {dados.ativa ? 'ativa' : 'inativa'}</span></p>
-  <p>Total gasto: {total}</p>
-  <p> {total > 10000 ? 'Voce esta gastando muito' : ''}  </p>
-  </div>
-  )
+  // return (
+  //   <section>
+  //     {dados.map(({id, nome, preco, cores}) =>(
+  //       <div key={id}>
+  //       <h1>{nome}</h1>
+  //       <p>{preco}</p>
+  //       <ul>
+  //         {cores.map((cor)=> (
+  //           <li style={{backgroundColor: cor, color: 'white'}} key={cor}>{cor}</li>
+  //         ))}
+  //       </ul>
+  //       </div>
+  //     ) )}
+
+  //   </section>
+  // )
+    
+  // EXERCICIES 1
+  // const dados = mario
+  // const total = dados.compras.map((item) => 
+  //   Number(item.preco.replace("R$", ""))
+  // ).reduce((a , b) => a+b)
+
+  // return ( <div>
+  // <p>Nome: {dados.cliente} </p> 
+  // <p>Idade: {dados.idade} </p>
+  // <p>Situaçao: <span style={{ color: dados.ativa ? 'green' : 'red'}}> {dados.ativa ? 'ativa' : 'inativa'}</span></p>
+  // <p>Total gasto: {total}</p>
+  // <p> {total > 10000 ? 'Voce esta gastando muito' : ''}  </p>
+  // </div>
+  // )
 }
+
+ 
 
 export default App
