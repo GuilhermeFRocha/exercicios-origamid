b10c784172685ec7088baef1727c01466bdfb0f4
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-02-15 19:54:01 -0300

    useContext

---
+++ src/useContext/Produto.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { GlobalContext } from './GlobalStorage'
+
+const Produto = () => {
+
+  const global = React.useContext(GlobalContext)
+
+  function handleClick () {
+    global.setCarrinho((carrinho) => {
+      carrinho +1
+    })
+  }
+
+  return (
+    <p>Total: {global.carrinho}: <button onClick={handleClick}></button></p>
+  )
+}
+
+export default Produto
