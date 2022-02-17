d5fef17513cd6069187e4c6fe1a5f44f3c1c4ce7
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-02-17 13:23:07 -0300

    checkbox

--- src/App.js
+++ src/App.js
@@ -1,47 +1,10 @@
 import React from "react";
-const coresArray = ['azul', 'roxo', 'laranja', 'verde', 'vermelho', 'cinza']
 
 
 const App = () => {
 
-  const [cores, setCores]  = React.useState([])
-
-  function handleChange ({target}) {
-    const {checket,value} = target;
-
-    if (checked) {
-      setCores([...cores, target.value]);
-    } else {
-      setCores(cores.filter((cor) =>{
-        cor !== value
-      }))
-    }
-  }
-  
-  function handleChecked (cor) {
-    return cores.includes(cor)
-  }
-
 return (
-  <form>
-    {coresArray.map((cor) => (
-      <label key={cor}>
-        <input 
-        type="checkbox" 
-        value={cor}
-        checked={handleChecked(cor)}
-        onChange={handleChange}
-        />
-        {cor}
-      </label>
-    ))}
-
-    <ul>
-      {crossOriginIsolated.map((cor) => (
-        <li key={cor}>{cor}</li>
-      ))}
-    </ul>
-  </form>
 )
+
 }
 export default App
