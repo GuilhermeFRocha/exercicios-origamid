28ebc611da5f39109c0fa9c0d786adb5c2d993c5
GuilhermeFRocha <guilhermefdarocha@outlook.com>
2022-02-21 12:38:46 -0300

    form

--- src/Validacao/App.js
+++ src/Validacao/App.js
@@ -2,30 +2,10 @@ import React from "react";
 import Input from './Form/Input'
 
 const App = () => {
-  const [cep, setCep] = React.useState('')
-  const [error, setError] = React.useState(null)
+  const cep = useForm('cep')
+ 
 
-  function validateCep(value) {
-    if(value.length === 0) {
-      setError('Preencha um valor')
-      return false
-    } else if(!/^\d{5}-?\d{3}$/.test(value)){
-      setError('Preencha um CEP valido')
-      return false
-    } else {
-      setError(null)
-      return true
-    }
-  }
-
-  function handleBlur ({target}) {
-    validateCep(target.value)
-  }
-
-  function handleChange ({target}) {
-    if (error)  validateCep(target.value)
-    setCep(target.value)
-  }
+  
 
   function handleSubmit (event) {
     event.preventDefault();
